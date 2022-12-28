03c509858426b4f5c8802b4c3affd4f46d4ca301
David Gaspar <davidgaspar.dev@gmail.com>
2022-12-27 23:30:04 -0300

    chore: create blank fragment to instruction

---
+++ app/src/main/java/com/udacity/shoestore/screens/instruction/InstructionFragment.kt
@@ -0,0 +1,30 @@
+package com.udacity.shoestore.screens.instruction
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
+import com.udacity.shoestore.R
+import com.udacity.shoestore.databinding.FragmentInstructionBinding
+
+class InstructionFragment : Fragment() {
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        // Inflate the layout for this fragment
+        val binding =  bindingInflater(inflater, container)
+
+        return binding.root
+    }
+
+    private fun bindingInflater(inflater: LayoutInflater, container: ViewGroup?)
+        = DataBindingUtil.inflate<FragmentInstructionBinding>(
+            inflater,
+            R.layout.fragment_instruction,
+            container,
+            false
+        )
+}
