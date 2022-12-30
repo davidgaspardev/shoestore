af845099f66708e3a0f6adcc89663a749d2fd34a
David Gaspar <davidgaspar.dev@gmail.com>
2022-12-30 09:46:45 -0300

    chore: add mock data (ShoeList)

--- app/src/main/java/com/udacity/shoestore/screens/shoeList/ShoeListViewModel.kt
+++ app/src/main/java/com/udacity/shoestore/screens/shoeList/ShoeListViewModel.kt
@@ -11,5 +11,40 @@ class ShoeListViewModel: ViewModel() {
     val shoeList: LiveData<List<Shoe>>
         get() = _shoeList
 
+    init {
+        val shoeList = listOf(
+            Shoe(
+                "All Colors Star",
+                38.0,
+                "NetShoes",
+                "Shoe All Colors Star Unissex Old Fnx"
+            ),
+            Shoe(
+                "Venetto",
+                41.0,
+                "NetShoes",
+                "Tênis Venetto Masculino Academia Fitness Leve Confortável"
+            ),
+            Shoe(
+                "Salomon Speedcross 4 GTX",
+                40.0,
+                "SOLOMON",
+                "SALOMON Men's Speedcross 4 GTX Trail Running Shoes Waterproof"
+            ),
+            Shoe(
+                "Gel-Kayano 29",
+                42.0,
+                "ASICS",
+                "ASICS Men's Gel-Kayano 29 Sneaker"
+            ),
+            Shoe(
+                "Pink JW4005",
+                37.0,
+                "Jack Walker",
+                "Jack Walker Women Waterproof Hiking Boots Lightweight Trekking Walking Shoes Pink JW4005"
+            )
+        )
 
+        this._shoeList.value = shoeList
+    }
 }
