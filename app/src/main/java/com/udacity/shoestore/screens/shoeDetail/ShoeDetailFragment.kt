1ef0b2eb3dd04036ba08bdcf2ee4729327965aab
David Gaspar <davidgaspar.dev@gmail.com>
2022-12-31 15:44:46 -0300

    chore: implement navigation to ShoeDetail

--- app/src/main/java/com/udacity/shoestore/screens/shoeDetail/ShoeDetailFragment.kt
+++ app/src/main/java/com/udacity/shoestore/screens/shoeDetail/ShoeDetailFragment.kt
@@ -4,11 +4,15 @@ import android.os.Bundle
 import android.view.View
 import android.view.ViewGroup
 import android.view.LayoutInflater
+import android.widget.Toast
 import androidx.fragment.app.Fragment
 import androidx.databinding.DataBindingUtil
+import androidx.navigation.fragment.findNavController
 
 import com.udacity.shoestore.R
 import com.udacity.shoestore.databinding.FragmentShoeDetailBinding
+import com.udacity.shoestore.models.Shoe
+import timber.log.Timber
 
 class ShoeDetailFragment: Fragment() {
 
@@ -19,7 +23,37 @@ class ShoeDetailFragment: Fragment() {
     ): View {
         val binding = bindingInflater(inflater, container)
 
+        binding.buttonCancelShoe.setOnClickListener {
+            findNavController().popBackStack()
+        }
+
+        binding.buttonSaveShoe.setOnClickListener {
+            try {
+                val shoeName = binding.editTextShoeName.text.toString()
+                val company = binding.editTextCompany.text.toString()
+                val shoeSize = binding.editTextShoeSize.text.toString()
+                val description = binding.editTextDescription.text.toString()
+
+                if (shoeName.isEmpty() || company.isEmpty() || shoeSize.isEmpty() || description.isEmpty()) {
+                    throw Exception("please fill in all fields")
+                }
+
+                val shoe = Shoe(shoeName, shoeSize.toDouble(), company, description)
+
+                val action = ShoeDetailFragmentDirections.actionBackToShoeList()
+                findNavController().navigate(action)
+            } catch (err: Exception) {
+                Timber.e(err)
+
+                alertInfo(err.message ?: err.toString())
+            }
+        }
+
         return binding.root
+    }
+
+    private fun alertInfo(info: String) {
+        Toast.makeText(context, info, Toast.LENGTH_SHORT).show()
     }
 
     private fun bindingInflater(inflater: LayoutInflater, container: ViewGroup?)
