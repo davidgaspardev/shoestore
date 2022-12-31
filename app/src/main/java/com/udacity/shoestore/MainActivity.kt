f5a151c96ff5aa1cd70437c030cb464f54741a80
David Gaspar <davidgaspar.dev@gmail.com>
2022-12-31 17:31:34 -0300

    chore: add logout menu to ShoeList screen

--- app/src/main/java/com/udacity/shoestore/MainActivity.kt
+++ app/src/main/java/com/udacity/shoestore/MainActivity.kt
@@ -2,11 +2,14 @@ package com.udacity.shoestore
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Menu
+import androidx.appcompat.widget.Toolbar
 import androidx.databinding.DataBindingUtil
 import androidx.navigation.fragment.NavHostFragment
 import androidx.navigation.ui.AppBarConfiguration
 import androidx.navigation.ui.setupWithNavController
 import com.udacity.shoestore.databinding.ActivityMainBinding
+import kotlinx.android.synthetic.main.activity_main.*
 import timber.log.Timber
 
 class MainActivity : AppCompatActivity() {
@@ -15,8 +18,11 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         val binding: ActivityMainBinding = DataBindingUtil.setContentView(this, R.layout.activity_main)
 
+        setSupportActionBar(binding.toolbar)
+
         val navController = (supportFragmentManager.findFragmentById(R.id.nav_host_fragment) as NavHostFragment).navController
         binding.toolbar.setupWithNavController(navController, AppBarConfiguration(navController.graph))
+
         Timber.plant(Timber.DebugTree())
     }
 }
--- app/src/main/java/com/udacity/shoestore/screens/shoeList/ShoeListFragment.kt
+++ app/src/main/java/com/udacity/shoestore/screens/shoeList/ShoeListFragment.kt
@@ -1,9 +1,7 @@
 package com.udacity.shoestore.screens.shoeList
 
 import android.os.Bundle
-import android.view.View
-import android.view.ViewGroup
-import android.view.LayoutInflater
+import android.view.*
 import android.widget.Toast
 import androidx.fragment.app.Fragment
 import androidx.databinding.DataBindingUtil
@@ -17,17 +15,23 @@ import com.udacity.shoestore.models.ShoeListViewModel
 
 class ShoeListFragment : Fragment() {
 
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        // Here notify the fragment that it should participate in options menu handling.
+        setHasOptionsMenu(true)
+    }
+
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View {
         val binding = bindingInflater(inflater, container)
-
         val shoeListViewModel = ViewModelProvider(this).get(ShoeListViewModel::class.java)
 
         shoeListViewModel.shoeList.observe(viewLifecycleOwner, Observer {
-            Toast.makeText(context, "shoe list changed", Toast.LENGTH_SHORT).show()
+            Toast.makeText(context, "shoe list changed: ${it.size}", Toast.LENGTH_SHORT).show()
         })
 
         binding.buttonShoeDetail.setOnClickListener {
@@ -36,6 +40,22 @@ class ShoeListFragment : Fragment() {
         }
 
         return binding.root
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu, inflater: MenuInflater) {
+        menu.clear()
+        inflater.inflate(R.menu.menu_shoe_list, menu)
+        super.onCreateOptionsMenu(menu, inflater)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        when (item.itemId) {
+            R.id.menu_logout -> {
+                val action = ShoeListFragmentDirections.actionBackToLogin()
+                findNavController().navigate(action)
+            }
+        }
+        return super.onOptionsItemSelected(item)
     }
 
     private fun bindingInflater(inflater: LayoutInflater, container: ViewGroup?)
